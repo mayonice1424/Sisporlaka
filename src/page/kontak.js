9eb1b4984096f3e611239e5ce5080353de3ea9cc
mayonice1424 <friyandrailham14@gmail.com>
2023-04-09 04:10:43 +0700

    fixing login

--- src/page/kontak.js
+++ src/page/kontak.js
@@ -1,21 +1,63 @@
-import { Wrap, Flex, Text } from '@chakra-ui/react'
-import React from 'react'
+import { Wrap, Flex, Text, color } from '@chakra-ui/react'
+import {React,useState} from 'react'
 import {SiGooglemaps} from 'react-icons/si'
+import {FiPhone} from 'react-icons/fi'
 import Navbar from './navbar';
+import { icons } from 'react-icons';
+
 function Kontak() {
+  const [data, setData] = useState([
+    {
+      id: 1,
+      namaInstansi: 'Dinas Perhubungan',
+      alamat: 'Jl. Basuki Rahmat No.34, Sumur Putri, Tlk. Betung Utara, Kota Bandar Lampung, Lampung 35211',
+      warna: '#448FFF',
+      noTelp: '(0721) 471 633',
+    },
+    {
+      id: 2,
+      namaInstansi: 'Polresta Bandar Lampung',
+      alamat: ' Jl. Mayor Jenderal Mt Haryono No. 15, Kota Bandar Lampung, Lampung',
+      warna: '#000000',
+      noTelp: '(0721) 253110',
+    },
+    {
+      id: 3,
+      namaInstansi: 'Dinas Kesehatan',
+      alamat: 'Jl. Dr. Susilo No. 44 Bandar Lampung',
+      warna: '#44B04E',
+      noTelp: '(0721) 471 633',
+    },
+    {
+      id: 4,
+      namaInstansi: 'PT. Jasa Raharja',
+      alamat: 'Jalan Wolter Monginsidi No.220A, Pahoman, Teluk Betung Utara, Sumur Putri, Kec. Tlk. Betung Utara, Kota Bandar Lampung, Lampung 35213',
+      warna: '#159EDB',
+      noTelp: '(0721) 487522',
+    },
+  ]);
   return (
     <>
     <Navbar />
-    <Wrap marginLeft={'10'} width={'100%'} flexDir={'row'} marginTop={'15vh'} height={'85vh'} bg={'white'} overflowY={'scroll'} position={'absolute'} zIndex={'-1'}>
-       <Flex marginLeft={'5vh'} marginRight={'5vh'} >
-          <Flex flexDir={'column'}>
-            <Text pt={'10'} paddingBottom={'5'}>Dinas Perhubungan</Text>
+    <Wrap width={'100%'} flexDir={'column'} mx={'5%'} alignItems={'center'} marginTop={'15vh'} alignContent={'center'} height={'85vh'} bg={'white'} overflowY={'scroll'} position={'fixed'} zIndex={'-1'}>
+       {
+          data.map((item) => {
+            data.id = item.id;
+            return (
+            <Flex width={'50%'} flexDir={'column'}>
+              <Text pt={'10'} fontWeight={'medium'} paddingBottom={'5'}>{item.namaInstansi}</Text>
+            <Flex py={'2%'} flexDir={'row'}>
+              <SiGooglemaps color={item.warna} size={'30'} />
+                <Text fontWeight={'regular'} marginLeft={'2%'}>{item.alamat}</Text>
+            </Flex>
             <Flex flexDir={'row'}>
-              <SiGooglemaps color='#448FFF' size={'30'} />
-              <Text marginLeft={'2%'}>Jl. Jendral Sudirman No. 1, Kec. Kebayoran Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12110</Text>
+              <FiPhone color={item.warna} size={'30'} />
+                <Text marginLeft={'2%'}>{item.noTelp}</Text>
             </Flex>
           </Flex>
-       </Flex>
+            )
+          })
+       }
     </Wrap>
     </>
   )
--- src/page/login.js
+++ src/page/login.js
@@ -1,10 +1,94 @@
-import React from 'react';
-import { Flex } from '@chakra-ui/react';
+import React, { useEffect } from "react"
+import { Box, Flex,InputGroup, InputLeftElement, Button, Image, Text, Input } from "@chakra-ui/react"
+import { Formik, Form } from 'formik'
+import {FaUser} from 'react-icons/fa'
+import {RiLockPasswordFill} from 'react-icons/ri'
+import { FormControl, FormLabel, FormErrorMessage } from "@chakra-ui/form-control"
 
-const login = () => {
+
+const Login = () => {
   return (
-    <div>login</div>
+    <>
+    <Flex backgroundColor={"var(--color-on-primary)"} width='100%' height="100vh" alignItems='center' justifyContent='center'>
+      <Flex flexDir={'column'} textAlign={'center'} alignContent={'center'} backgroundColor={"var(--color-on-primary)"} width='100%' height="100%" alignItems='center' justifyContent='center' display={{ base: 'none', md: 'none', lg: 'flex' }}>
+        <Text position={'Relative'} fontWeight={'regular'} fontFamily={'var(--font-family-secondary)'} fontSize={'var(--header-1)'} width={'100%'} maxWidth={'400px'}> Sisporlaka</Text>
+        <Text  fontWeight={'regular'} fontFamily={'var(--font-family-secondary)'} fontSize={'var(--header-3)'} color={'{var(--color-primer)}'} >Sistem Informasi Pelaporan Kecelakaan</Text>
+        <Text py={4} fontWeight={'semibold'} fontFamily={'var(--font-family-secondary)'} fontSize={'var(--header-4)'} color={'{var(--color-primer)}'} >Kolaborasi Pilar Lalu Lintas dan Angkutan Jalan Menuju 1 Data Kecelakaan </Text>
+        <Image
+						  width={"25%"}
+							position={'Relative'} maxWidth={'350px'} 
+							src={process.env.PUBLIC_URL + '/logokolaborasi.png'}
+						/>
+      </Flex>
+      <Flex backgroundColor={{ lg: "var(--color-primer)" }} width='100%' height="100%" alignItems={{ lg: 'center' }} justifyContent='center'>
+        <Box max-width='800px' borderRadius={'20px'} display="flex" gap="40px" flexDirection={"column"} size={'md'} width={{ base: '100%', md: '80%' }} padding="90px 50px 90px 50px" backgroundColor={"var(--color-on-primary)"} justifyContent={{ lg: 'center' }} textAlign='center' alignItems='center'>
+          <Image
+						  sizes="md" display={{ base: 'flex', lg: 'none' }} position={'Relative'} width={'80%'} maxWidth={'200px'}
+							src={process.env.PUBLIC_URL + '/logokolaborasi.png'}
+						/>
+          <Text size='md' fontWeight='bold' fontFamily='var(--font-family-secondary)' fontSize='var(--header-2)' color='var(--color-primer)' >Masuk</Text>
+          <Formik
+           
+          >
+            {() => (
+              <Form>
+                <FormControl width={{ base: '100%', md: '100%', lg:'380px' }} size={'md'} marginTop={'20px'}>
+                  <FormLabel htmlFor="username">
+                    Username
+                  </FormLabel>
+                  <InputGroup>
+                    <InputLeftElement
+                       pointerEvents='none'
+                       children={<FaUser color='gray.300' />}
+                   />
+                  <Input size={'md'}
+                    marginTop={'0 auto'}
+                    type="text"
+                    name="username"
+                    variant='outline'
+                    placeholder="Username"
+                    />
+                  </InputGroup>
+                </FormControl>
+                <FormControl size={'md'} marginTop={'20px'}>
+                  <FormLabel htmlFor="password">
+                    Password
+                  </FormLabel>
+                  <InputGroup>
+                    <InputLeftElement
+                       pointerEvents='none'
+                       children={<RiLockPasswordFill size={'20'} color='gray.300' />}
+                   />
+                  <Input size={'md'}
+                    marginTop={'0 auto'}
+                    type="text"
+                    name="password"
+                    variant='outline'
+                    placeholder="Password"
+                    />
+                  </InputGroup>
+                </FormControl>
+                <Button
+                  marginTop={'44px'}
+                  width="100%"
+                  height="50px"
+                  borderRadius="10px"
+                  backgroundColor="var(--color-primer)"
+									loadingText="Tunggu Sebentar..."
+                  type="submit"
+                  className="btn-login"
+                  >
+                  <Text fontWeight='bold' fontFamily='var(--font-family-secondary)' fontSize='var(--header-3)' color='var(--color-on-primary)' >
+                    Masuk
+                  </Text>
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Flex>
+    </Flex>
+ </>
   )
-}
-
-export default login
+};
+export default Login
